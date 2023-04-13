218ad6ca9254212dcd049666b0945f4d01a65a59
evanstcom <zhenyakoks@gmail.com>
2023-04-13 15:12:43 +0300

    add navbar

--- src/pages/film/[id].tsx
+++ src/pages/film/[id].tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Layout from '@/components/layout/Layout'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
-import './film.module.scss'
+import styles from './film.module.sass'
+import Link from 'next/link'
 
 const FilmPage: NextPage = () => {
 
@@ -10,12 +11,13 @@ const FilmPage: NextPage = () => {
 
   return (
     <Layout title="Фильм">
-      <main className='container'>FilmPage
-        <div>
-          <ul>
-            <li>Фильмы</li>
-            <li>Документальные</li>
+      <main className={`container ${styles.film__container}`}>
+        <div className={styles.film__navbar}>
+          <ul className={styles.film__navigation}>
+            <li><Link href={'/movies'}>Фильмы</Link></li>
+            <li><Link href={asPath}>Название</Link></li>
           </ul>
+          <div className={styles.film__badge}>бесплатно</div>
         </div>
 
       </main>
