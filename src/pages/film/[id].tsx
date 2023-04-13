e0de695580b15fa690d42077fe73c085e99983ca
evanstcom <zhenyakoks@gmail.com>
2023-04-13 13:46:14 +0300

    add pages movies and film

--- src/pages/film/[id].tsx
+++ src/pages/film/[id].tsx
@@ -2,16 +2,25 @@ import React from 'react'
 import Layout from '@/components/layout/Layout'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import './film.module.scss'
 
 const FilmPage: NextPage = () => {
 
   const { asPath, pathname } = useRouter()
 
   return (
-    <Layout title="Онлайн-кинотеатр Иви">
-      <div>FilmPage</div>
+    <Layout title="Фильм">
+      <main className='container'>FilmPage
+        <div>
+          <ul>
+            <li>Фильмы</li>
+            <li>Документальные</li>
+          </ul>
+        </div>
+
+      </main>
     </Layout>
-    
+
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 // components
 import Home from '@/components/screens/home/Home'
+import Movies from '@/components/screens/home/Home'
 
 export default function HomePage() {
   return (
