392007aab24604001923bff76a339565ab2899d8
enjeru <tray.shooter@mail.ru>
2023-04-18 02:19:50 +0300

    upd

--- src/functions/truncateText.ts
+++ src/functions/truncateText.ts
@@ -1,12 +1,16 @@
 export function truncateText(text: string, maxLength: number, typeOfTruncate?: string) {
   if (text.length <= maxLength) return text
-  let result = ''
 
-  for (let word of text.split(' ')) {
-    if ((result + ' ' + word).length >= maxLength) break
-    result += ` ${word}`
+  if (typeOfTruncate === 'break-word') {
+    return text.slice(0, maxLength) + '...'
+  } else {
+    let result = ''
+
+    for (let word of text.split(' ')) {
+      if ((result + ' ' + word).length >= maxLength) break
+      result += ` ${word}`
+    }
+    return result + '...'
   }
 
-  if (result === '') result = text.slice(0, maxLength)
-  return result + '...'
 }
