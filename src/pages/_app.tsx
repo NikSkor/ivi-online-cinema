227e9b61928a5667a842427e9e292b0e26820553
NikSkor <mr-zerx@yandex.ru>
2023-04-22 21:36:23 +0500

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,9 @@ import { store } from '@/store/store'
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Provider store={store}>
-           <Component {...pageProps} />
-         </Provider>
-
+  return(
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
